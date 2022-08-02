003c9a369da3cbb318f1f0f20ecf532134ce504d
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2022-08-02 13:13:12 +0200

    calling protection created (name == main)

--- ontcatowl.py
+++ ontcatowl.py
@@ -1,46 +1,42 @@
 """Main module for OntCatOWL"""
 
-import logging
+if __name__ == "__main__":
 
-from rdflib import Graph
+    import logging
 
-from ontcatowl_dataclasses import initialize_gufo_list
+    from rdflib import Graph
 
-# TODO (@pedropaulofb): Set base level for printing log
-logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.DEBUG)
+    from modules.ontcatowl_dataclasses import initialize_gufo_list
 
-gufo = Graph()
-ontology = Graph()
+    # TODO (@pedropaulofb): Set base level for printing log
+    logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.DEBUG)
 
-# Input GUFO ontology
-# TODO (@pedropaulofb): Change for the complete version of GUFO after the tests are finished.
-try:
-    gufo.parse("resources/gufoEndurantsOnly.ttl")
-except OSError:
-    logging.error("Could not load resources/gufoEndurantsOnly.ttl file. Exiting program.")
-    exit(1)
+    gufo = Graph()
+    ontology = Graph()
 
-# TODO (@pedropaulofb): Read from argument
-# Input ontology to be evaluated
-try:
-    ontology.parse("resources/d3fend.ttl")
-except OSError:
-    logging.error("Could not load resources/d3fend.ttl file. Exiting program.")
-    exit(1)
+    # Input GUFO ontology
+    # TODO (@pedropaulofb): Change for the complete version of GUFO after the tests are finished.
+    try:
+        gufo.parse("resources/gufoEndurantsOnly.ttl")
+    except OSError:
+        logging.error("Could not load resources/gufoEndurantsOnly.ttl file. Exiting program.")
+        exit(1)
 
-# TODO (@pedropaulofb): Read all classes from input ontology and create a list with no repetitions
+    # TODO (@pedropaulofb): Read from argument
+    # Input ontology to be evaluated
+    try:
+        ontology.parse("resources/d3fend.ttl")
+    except OSError:
+        logging.error("Could not load resources/d3fend.ttl file. Exiting program.")
+        exit(1)
 
-logging.debug("Initializing RDFS inferences. This may take a while...")
-# DeductiveClosure(RDFS_Semantics).expand(ontology)  # Performs RDFS inferences
+    # TODO (@pedropaulofb): Read all classes from input ontology and create a list with no repetitions
 
-logging.debug("Initializing list of GUFO concepts.")
-gufo_list = []
-initialize_gufo_list(gufo_list)
+    logging.debug("Initializing RDFS inferences. This may take a while...")
+    # DeductiveClosure(RDFS_Semantics).expand(ontology)  # Performs RDFS inferences
 
-# TODO (@pedropaulofb): Create log file parallel to logs printed on std.out
-#  (e.g., https://github.com/borntyping/jsonlog)
-# TODO (@pedropaulofb): Use different colors for logs levels printed on std.ou
-#  (e.g. https://betterstack.com/community/questions/how-to-color-python-logging-output/)
-# TODO (@pedropaulofb): Future argument options: save in one file (ont + gufo), save inferences as assertions
-# TODO (@pedropaulofb): Evaluate on Linux before release first version
-# TODO (@pedropaulofb): update requirements.txt
+    logging.debug("Initializing list of GUFO concepts.")
+    gufo_list = []
+    initialize_gufo_list(gufo_list)
+
+    # TODO (@pedropaulofb): Create log file parallel to logs printed on std.out  #  (e.g., https://github.com/borntyping/jsonlog)  # TODO (@pedropaulofb): Use different colors for logs levels printed on std.ou  #  (e.g. https://betterstack.com/community/questions/how-to-color-python-logging-output/)  # TODO (@pedropaulofb): Future argument options: save in one file (ont + gufo), save inferences as assertions  # TODO (@pedropaulofb): Evaluate on Linux before release first version  # TODO (@pedropaulofb): update requirements.txt
