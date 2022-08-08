ac95917d363523a7aab9be386f70aa9b5f75bc2a
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2022-08-08 13:15:36 +0200

    GUFO graph is not imported anymore

--- ontcatowl.py
+++ ontcatowl.py
@@ -13,16 +13,7 @@ if __name__ == "__main__":
     #   e.g., print debut only to file
     logging.basicConfig(format='%(levelname)s - %(message)s', level=logging.DEBUG)
 
-    gufo = Graph()
     ontology = Graph()
-
-    # Input GUFO ontology
-    # TODO (@pedropaulofb): Change for the complete version of GUFO after the tests are finished.
-    try:
-        gufo.parse("resources/gufoEndurantsOnly.ttl")
-    except OSError:
-        logging.error("Could not load resources/gufoEndurantsOnly.ttl file. Exiting program.")
-        exit(1)
 
     # TODO (@pedropaulofb): Read from argument
     # Input ontology to be evaluated
@@ -62,6 +53,7 @@ if __name__ == "__main__":
 #       (e.g. https://betterstack.com/community/questions/how-to-color-python-logging-output/)
 # TODO (@pedropaulofb): Future argument options: save in one file (ont + gufo), save inferences as assertions
 # TODO (@pedropaulofb): Verify possibility to check consistency using a reasoner.
+# TODO (@pedropaulofb): Is there a way to define the GUFO list as read-only?
 # TODO (@pedropaulofb): Evaluate on Linux before release first version
 # TODO (@pedropaulofb): Update requirements.txt
 # TODO (@pedropaulofb): OntCatOWL can became a generic mapper tool!
