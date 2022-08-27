1d148bc983ce48e79bf58bb2ba79ad404cba4dee
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2022-08-27 17:43:40 +0200

    rules_actions improved
    Signed-off-by: Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>

--- modules/rules_actions.py
+++ modules/rules_actions.py
@@ -9,40 +9,37 @@ def perform_rule_actions_types(list_ontology_dataclasses, list_nodes, list_actio
 
     if list_restrictions is None:
         list_restrictions = []
+
     logger = initialize_logger()
 
     for action in range(len(list_actions_code)):
-        for node in range(len(list_nodes)):
-            for ontology_dataclass in range(len(list_ontology_dataclasses)):
+        for ontology_dataclass in range(len(list_ontology_dataclasses)):
 
-                # Condition 1
-                if list_ontology_dataclasses[ontology_dataclass].uri != list_nodes[node]:
-                    continue
+            # Condition 1: ontology dataclass must be in the list of nodes
+            if list_ontology_dataclasses[ontology_dataclass].uri not in list_nodes:
+                continue
 
-                # Condition 2
-                if list_ontology_dataclasses[ontology_dataclass].uri not in list_restrictions:
+            # Condition 2: ontology dataclass must not be in the list of restrictions
+            if list_ontology_dataclasses[ontology_dataclass].uri not in list_restrictions:
 
-                    logger.debug(
-                        f"Executing {list_actions_code[action]} in {list_ontology_dataclasses[ontology_dataclass]}...")
+                logger.debug(
+                    f"Executing {list_actions_code[action]} in {list_ontology_dataclasses[ontology_dataclass]}...")
 
-                    if list_actions_code[action] == "rule_t1":
-                        list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:Sortal")
+                # Rules t1 and t5
+                if (list_actions_code[action] == "rule_t1") or (list_actions_code[action] == "rule_t5"):
+                    list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:Sortal")
 
-                    if list_actions_code[action] == "rule_t2":
-                        list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:NonSortal")
+                # Rule t2
+                if list_actions_code[action] == "rule_t2":
+                    list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:NonSortal")
 
-                    if list_actions_code[action] == "rule_t3":
-                        list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:Kind")
+                # Rules t3 and t4
+                if (list_actions_code[action] == "rule_t3") or (list_actions_code[action] == "rule_t4"):
+                    list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:Kind")
 
-                    if list_actions_code[action] == "rule_t4":
-                        list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:Kind")
+                # Rule t6
+                if list_actions_code[action] == "rule_t6":
+                    list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:AntiRigidType")
 
-                    if list_actions_code[action] == "rule_t5":
-                        list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:Sortal")
-
-                    if list_actions_code[action] == "rule_t6":
-                        list_ontology_dataclasses[ontology_dataclass].move_element_to_not_list("gufo:AntiRigidType")
-
-                    logger.debug(
-                        f"Successfully executed {list_actions_code[action]} "
-                        f"in {list_ontology_dataclasses[ontology_dataclass]}.")
+                logger.debug(f"Successfully executed {list_actions_code[action]} "
+                             f"in {list_ontology_dataclasses[ontology_dataclass]}.")
