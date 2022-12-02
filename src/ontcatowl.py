12ea6c951270496985e4e5e1274b4d6b64cd7c30
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2022-12-02 11:29:03 +0100

    init correction
    Signed-off-by: Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>

--- src/ontcatowl.py
+++ src/ontcatowl.py
@@ -22,7 +22,7 @@ from src.modules.utils_rdf import load_all_graph_safely, perform_reasoning, load
 
 SOFTWARE_ACRONYM = "OntCatOWL"
 SOFTWARE_NAME = "Identification of Ontological Categories for OWL Ontologies"
-SOFTWARE_VERSION = "0.22.11.22"
+SOFTWARE_VERSION = "0.22.12.02"
 SOFTWARE_URL = "https://github.com/unibz-core/OntCatOWL/"
 VERSION_RESTRICTION = "TYPES_ONLY"
 LIST_GRAPH_RESTRICTIONS = [RDF.type, RDFS.subClassOf]
