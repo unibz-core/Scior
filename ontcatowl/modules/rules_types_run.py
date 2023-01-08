10fea9bd4ba65be95a811ed191ccfb609fcc0b08
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-01-08 15:35:20 +0100

    minor correction on screen message
    Signed-off-by: Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>

--- ontcatowl/modules/rules_types_run.py
+++ ontcatowl/modules/rules_types_run.py
@@ -12,7 +12,7 @@ from ontcatowl.modules.utils_dataclass import generate_hash_ontology_dataclass_l
 def execute_rules_types(ontology_dataclass_list, graph, nodes_list, configurations):
     """ Executes all rules related to types. """
     logger = initialize_logger()
-    logger.info("Starting GUFO types hierarchy rules ...")
+    logger.info("Starting gUFO types' hierarchy rules ...")
 
     # Rules
     always_automatic_rules = ["k_s_sup", "s_k_sub", "t_k_sup", "ns_s_sup", "s_ns_sub", "r_ar_sup", "ar_r_sub",
