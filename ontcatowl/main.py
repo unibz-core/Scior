05c183b336e7f8c3553f1add01b0e420fa884efe
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-01-09 11:28:31 +0100

    SOFTWARE_VERSION added as return value of the run_ontcatowl_tester function.
    Signed-off-by: Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>

--- ontcatowl/main.py
+++ ontcatowl/main.py
@@ -24,7 +24,7 @@ from ontcatowl.modules.utils_rdf import load_all_graph_safely, perform_reasoning
 
 SOFTWARE_ACRONYM = "OntCatOWL"
 SOFTWARE_NAME = "Identification of Ontological Categories for OWL Ontologies"
-SOFTWARE_VERSION = "0.23.01.07"
+SOFTWARE_VERSION = "0.23.01.09"
 SOFTWARE_URL = "https://github.com/unibz-core/OntCatOWL/"
 VERSION_RESTRICTION = "TYPES_ONLY"
 LIST_GRAPH_RESTRICTIONS = [RDF.type, RDFS.subClassOf]
@@ -147,7 +147,7 @@ def run_ontcatowl_tester(global_configurations, working_graph):
     except Exception:
         exit(1)
 
-    return ontology_dataclass_list, time_register, consolidated_statistics, knowledge_matrix
+    return ontology_dataclass_list, time_register, consolidated_statistics, knowledge_matrix, SOFTWARE_VERSION
 
 
 if __name__ == "__main__":
