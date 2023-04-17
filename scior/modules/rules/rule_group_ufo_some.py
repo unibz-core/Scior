3d58d99d7fa7fa03a1be8440c007f821b222187d
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-04-17 22:16:00 +0200

    rule 35 implemented

--- scior/modules/rules/rule_group_ufo_some.py
+++ scior/modules/rules/rule_group_ufo_some.py
@@ -1,5 +1,5 @@
 """ Implementation of rules of group UFO Some. """
-from rdflib import Graph
+from rdflib import Graph, URIRef, RDFS
 
 from scior.modules.dataclass_definitions_ontology import OntologyDataClass
 from scior.modules.logger_config import initialize_logger
@@ -8,7 +8,7 @@ from scior.modules.utils_deficiencies import register_incompleteness, report_err
 
 LOGGER = initialize_logger()
 
-
+# TODO (@pedropaulofb): Modify selected_dataclass to INCOMPLETE_CLASSES (and use all classes from the left side of the definition)! RULE 35 already OK!
 def treat_result_ufo_some(ontology_dataclass_list: list[OntologyDataClass], selected_dataclass: OntologyDataClass,
                           can_classes_list: list[str], is_classes_list: list[str], types_to_set_list: list[str],
                           rule_code: str, arguments: dict) -> None:
@@ -61,7 +61,7 @@ def run_r24rg(ontology_dataclass_list: list[OntologyDataClass], ontology_graph: 
     Code: R24Rg
     Definition: AntiRigidType(x) ^ Sortal(x) ^ Category(y) ^ subClassOf(x,y) ->
                     E z (RigidType(z) ^ Sortal(z) ^ subClassOf(x,z) ^ subClassOf(z,y))
-    Description: AntiRigid Sortals cannot "only directly specialize" Categories.
+    Description: AntiRigid Sortals cannot "directly specialize" Categories. This must be done through a Ridig Sortal.
     """
 
     rule_code = "R24Rg"
@@ -82,10 +82,10 @@ def run_r24rg(ontology_dataclass_list: list[OntologyDataClass], ontology_graph: 
 
     query_result = ontology_graph.query(query_string)
 
-    is_list = []
-    can_list = []
-
     for row in query_result:
+
+        is_list = []
+        can_list = []
 
         selected_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_z.toPython())
 
@@ -128,10 +128,10 @@ def run_r25rg1(ontology_dataclass_list: list[OntologyDataClass], ontology_graph:
 
     query_result = ontology_graph.query(query_string)
 
-    is_list = []
-    can_list = []
-
     for row in query_result:
+
+        is_list = []
+        can_list = []
 
         selected_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_y.toPython())
 
@@ -174,10 +174,10 @@ def run_r25rg2(ontology_dataclass_list: list[OntologyDataClass], ontology_graph:
 
     query_result = ontology_graph.query(query_string)
 
-    is_list = []
-    can_list = []
-
     for row in query_result:
+
+        is_list = []
+        can_list = []
 
         selected_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_y.toPython())
 
@@ -194,6 +194,114 @@ def run_r25rg2(ontology_dataclass_list: list[OntologyDataClass], ontology_graph:
 
         treat_result_ufo_some(ontology_dataclass_list, selected_dataclass, can_list, is_list, ["AntiRigidType"],
                               rule_code, arguments)
+
+    LOGGER.debug(f"Rule {rule_code} concluded.")
+
+
+def run_r34rg(ontology_dataclass_list: list[OntologyDataClass], ontology_graph: Graph, arguments: dict):
+    """ Executes rule R34Rg from group UFO Some.
+
+    Code: R34Rg
+    Definition: Role(x) ^ PhaseMixin(y) ^ subClassOf(x,y) -> Es z (Phase(z) ^ subClassOf(x,z) ^ subClassOf(z,y))
+    Description: A Role cannot "specialize directly"  a PhaseMixin. This must be done through a Phase.
+    """
+
+    rule_code = "R34Rg"
+
+    LOGGER.debug(f"Starting rule {rule_code}")
+
+    query_string = """
+        PREFIX gufo: <http://purl.org/nemo/gufo#>
+        SELECT DISTINCT ?class_x ?class_y ?class_z
+        WHERE {
+            ?class_x rdf:type gufo:Role .
+            ?class_y rdf:type gufo:PhaseMixin .
+            ?class_x rdfs:subClassOf ?class_y .
+            ?class_x rdfs:subClassOf ?class_z .
+            ?class_z rdfs:subClassOf ?class_y .
+        } """
+
+    query_result = ontology_graph.query(query_string)
+
+    for row in query_result:
+
+        is_list = []
+        can_list = []
+
+        selected_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_z.toPython())
+
+        if selected_dataclass is None:
+            report_error_dataclass_not_found(row.class_z.toPython())
+
+        # Creating IS List
+        if "Phase" in selected_dataclass.is_type:
+            is_list.append(selected_dataclass.uri)
+
+        # Creating CAN List
+        elif "Phase" in selected_dataclass.can_type:
+            can_list.append(selected_dataclass.uri)
+
+        treat_result_ufo_some(ontology_dataclass_list, selected_dataclass, can_list, is_list, ["Phase"],
+                              rule_code, arguments)
+
+    LOGGER.debug(f"Rule {rule_code} concluded.")
+
+
+def run_r35rg(ontology_dataclass_list: list[OntologyDataClass], ontology_graph: Graph, arguments: dict):
+    """ Executes rule R35Rg from group UFO Some.
+
+    Code: R35Rg
+    Definition: Phase(x) -> E y (Phase (y) ^ shareKind(x,y) ^ ~isSubClassOf(x,y) ^ ~isSubClassOf(y,x))
+    Description: There must exist at least two phases that shares the same Kind and that do not specialize each other.
+    """
+
+    rule_code = "R35Rg"
+
+    LOGGER.debug(f"Starting rule {rule_code}")
+
+    query_string = """
+            PREFIX gufo: <http://purl.org/nemo/gufo#> 
+            PREFIX scior: <https://purl.org/scior/>
+            SELECT DISTINCT ?class_x ?class_y
+            WHERE {
+                ?class_x rdf:type gufo:Phase .
+                ?class_x scior:shareKind ?class_y .
+            } """
+
+    query_result = ontology_graph.query(query_string)
+
+    for row in query_result:
+
+        is_list = []
+        can_list = []
+
+        if (URIRef(row.class_x.toPython()), RDFS.subClassOf, URIRef(row.class_y.toPython())) in ontology_graph:
+            is_subclass = True
+        else:
+            is_subclass = False
+
+        if (URIRef(row.class_y.toPython()), RDFS.subClassOf, URIRef(row.class_x.toPython())) in ontology_graph:
+            is_superclass = True
+        else:
+            is_superclass = False
+
+        if not is_subclass and not is_superclass:
+            source_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_x.toPython())
+            target_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_y.toPython())
+
+            if target_dataclass is None:
+                report_error_dataclass_not_found(row.class_y.toPython())
+
+            # Creating IS List
+            if "Phase" in target_dataclass.is_type:
+                is_list.append(target_dataclass.uri)
+
+            # Creating CAN List
+            elif "Phase" in target_dataclass.can_type:
+                can_list.append(target_dataclass.uri)
+
+            treat_result_ufo_some(ontology_dataclass_list, source_dataclass, can_list, is_list, ["Phase"],
+                                  rule_code, arguments)
 
     LOGGER.debug(f"Rule {rule_code} concluded.")
 
@@ -220,10 +328,10 @@ def run_r36rg(ontology_dataclass_list: list[OntologyDataClass], ontology_graph: 
 
     query_result = ontology_graph.query(query_string)
 
-    is_list = []
-    can_list = []
-
     for row in query_result:
+
+        is_list = []
+        can_list = []
 
         selected_dataclass = get_dataclass_by_uri(ontology_dataclass_list, row.class_y.toPython())
 
@@ -252,6 +360,8 @@ def execute_rules_ufo_some(ontology_dataclass_list, ontology_graph, arguments):
     run_r24rg(ontology_dataclass_list, ontology_graph, arguments)
     run_r25rg1(ontology_dataclass_list, ontology_graph, arguments)
     run_r25rg2(ontology_dataclass_list, ontology_graph, arguments)
+    run_r34rg(ontology_dataclass_list, ontology_graph, arguments)
+    run_r35rg(ontology_dataclass_list, ontology_graph, arguments)
     run_r36rg(ontology_dataclass_list, ontology_graph, arguments)
 
     LOGGER.debug("Execution of all rules from group UFO Some completed.")
